ee56cbc98c2ea70c7ba3663e374041c89569b39f
Andrii Vasyliev <sol@hiqdev.com>
2015-05-24 15:32:10 +0000

    minor

--- LightboxAsset.php
+++ LightboxAsset.php
@@ -9,18 +9,23 @@ namespace hiqdev\assets\lightbox2;
 
 class LightboxAsset extends \yii\web\AssetBundle
 {
+    /**
+     * @inheritdoc
+     */
     public $sourcePath = '@bower/lightbox2';
 
+    /**
+     * @inheritdoc
+     */
     public $js = [
         '/js/lightbox.min.js',
     ];
 
+    /**
+     * @inheritdoc
+     */
     public $css = [
         'css/lightbox.css'
-    ];
-
-    public $depends = [
-        'yii\web\JqueryAsset',
     ];
 
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Yii 2 asset for bower lightbox2
 -------------------------------
 
-Yii 2 asset for bower lightbox2
+Wrapper around bower package with copied versions
 
 For more info see wiki:
 https://github.com/hiqdev/yii2-asset-lightbox2/wiki
