89f09b37e4887e3edb561d2c77cb54fe0d96d805
Andrii Vasyliev <sol@hiqdev.com>
2015-08-28 08:54:44 +0000

    php-cs-fixed

--- src/LightboxAsset.php
+++ src/LightboxAsset.php
@@ -1,26 +1,34 @@
 <?php
 
+/*
+ * Yii 2 asset for bower LightBox2
+ *
+ * @link      https://github.com/hiqdev/yii2-asset-lightbox2
+ * @package   yii2-asset-lightbox2
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
 namespace hiqdev\assets\lightbox2;
 
 class LightboxAsset extends \yii\web\AssetBundle
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public $sourcePath = '@bower/lightbox2';
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public $js = [
         'js/lightbox.min.js',
     ];
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public $css = [
-        'css/lightbox.css'
+        'css/lightbox.css',
     ];
-
 }
